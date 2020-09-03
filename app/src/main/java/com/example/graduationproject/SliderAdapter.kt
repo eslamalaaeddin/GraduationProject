353c821ffff290ddbd664c9623781db24a3f155d
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-03 07:44:57 +0200

    sliding Adapter logic

---
+++ app/src/main/java/com/example/graduationproject/SliderAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.graduationproject
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.getSystemService
+import androidx.viewpager.widget.PagerAdapter
+import org.w3c.dom.Text
+
+val IMAGES = arrayOf(R.drawable.ic_travel_splash,R.drawable.ic_travel_splash,R.drawable.ic_travel_splash)
+val HEADINGS = arrayOf("Heading 1","Heading 2","Heading 3")
+val DESCRIPTIONS =
+    arrayOf("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+
+
+class SliderAdapter(val context: Context) : PagerAdapter() {
+    private lateinit var layoutInflater: LayoutInflater
+    override fun getCount(): Int {
+        return IMAGES.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view == `object` as LinearLayout
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater.inflate(R.layout.slide_layout,container,false)
+
+        val logoImageView : ImageView = view.findViewById(R.id.logo_image_view)
+        val headingTextView  : TextView = view.findViewById(R.id.heading_text_view)
+        val headingDescriptionTextView  : TextView = view.findViewById(R.id.heading_description_text_view)
+
+
+        logoImageView.setImageResource(IMAGES[position])
+        headingTextView.text = HEADINGS[position]
+        headingDescriptionTextView.text = DESCRIPTIONS[position]
+
+        container.addView(view)
+
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as LinearLayout)
+    }
+
+
+}
