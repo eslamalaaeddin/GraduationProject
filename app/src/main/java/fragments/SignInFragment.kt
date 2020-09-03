c787f263de83c5eb3403d02b0d5b12bf736aecc7
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-09-03 09:14:58 +0200

    text views listeners

--- app/src/main/java/fragments/SignInFragment.kt
+++ app/src/main/java/fragments/SignInFragment.kt
@@ -4,18 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.graduationproject.R
+import com.example.graduationproject.databinding.FragmentInSignBinding
+
 
 class SignInFragment : Fragment() {
+    private lateinit var signInBinding: FragmentInSignBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        signInBinding =
+            DataBindingUtil.inflate(inflater , R.layout.fragment_in_sign , container,false)
+
+        return signInBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        signInBinding.doNotHaveAccountTextView.setOnClickListener {
+
+            val signUpFragment = SignUpFragment()
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.fragment_container, signUpFragment)
+                ?.commit()
+
+        }
+
+        signInBinding.forgetPasswordTextView.setOnClickListener {
+
+        }
+
     }
 }
--- app/src/main/java/fragments/SignUpFragment.kt
+++ app/src/main/java/fragments/SignUpFragment.kt
@@ -4,19 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.graduationproject.R
+import com.example.graduationproject.databinding.FragmentUpSignBinding
 
 class SignUpFragment : Fragment() {
-
+    private lateinit var signUpBinding: FragmentUpSignBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        signUpBinding =
+            DataBindingUtil.inflate(inflater , R.layout.fragment_up_sign , container,false)
+
+        return signUpBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        signUpBinding.haveAccountTextView.setOnClickListener {
+
+            val signInFragment = SignInFragment()
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.fragment_container, signInFragment)
+                ?.commit()
+
+        }
     }
 }
